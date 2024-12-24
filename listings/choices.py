5ba133d087222027b0056c56674cc5d129e84382
lws <pythonlws@gmail.com>
2024-12-24 17:36:30 +0800

    step 8 : listings view

---
+++ listings/choices.py
@@ -0,0 +1,45 @@
+bedroom_choices = {
+    '1' : 1,
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '9' : 9,
+    '10' : 10,
+}
+
+price_choices = {
+    "1000000" : "$1,000,000",
+    "2000000" : "$1,000,000",
+    "3000000" : "$1,000,000",
+    "4000000" : "$1,000,000",
+    "5000000" : "$1,000,000",
+    "6000000" : "$1,000,000",
+    "7000000" : "$1,000,000",
+    "8000000" : "$1,000,000",
+    "9000000" : "$1,000,000",
+    "10000000" : "$10M+",
+}
+
+district_choices = {
+    "IL" : "Islands",
+    "SK" : "Sai Kung",
+    "NR" : "North",
+    "KT" : "Kwai Tsing",
+    "ST" : "Sha Tin",
+    "TP" : "Tai Po",
+    "TW" : "Tsuen Wan",
+    "TM" : "Tuen Mun",
+    "YL" : "Yuen Long",
+    "KC" : "Kowloon City",
+    "KW" : "Kwun Tong",
+    "SS" : "Sham Shui Po",
+    "WT" : "Wong Tai Sin",
+    "YM" : "Yau Tsim Mong",
+    "CW" : "Central & Western",
+    "ER" : "Eastern",
+    "SR" : "Southern",
+    "WC" : "Wan Chai",
+}
